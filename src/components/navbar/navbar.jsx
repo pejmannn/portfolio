fec5dac1770aa73d714971af57a0ffa46ea90901
artean <arrteann@gmail.com>
2024-03-23 13:01:44 +0100

    refactor: seperate svg icons into the file

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,167 +1,49 @@
 import "./navbar.css";
+
+const navs = [
+  {
+    name: "home",
+    link: "/",
+    icon: "/icons/home.svg",
+  },
+  {
+    name: "about",
+    link: "/about-me",
+    icon: "/icons/person.svg",
+  },
+  {
+    name: "contact",
+    link: "/contact-me",
+    icon: "/icons/call.svg",
+  },
+];
+
+
+// "
+// flex justify-around items-center m-auto 2xl:w-1/4 2xl:h-20 bg-nav rounded-full fixed left-0 right-0 bottom-12
+//     xl:h-12 xl:w-1/4
+//     lg:h-10 lg:w-1/4
+//     md:h-9 md:w-1/4
+//     sm:h-8 sm:w-1/4
+// "
+
 const Navbar = () => {
   return (
-    <div
-      className="flex justify-around items-center m-auto 2xl:w-1/4 2xl:h-20 bg-nav rounded-full fixed left-0 right-0 bottom-12
-    xl:h-12 xl:w-1/4
-    lg:h-10 lg:w-1/4
-    md:h-9 md:w-1/4
-    sm:h-8 sm:w-1/4
-    h-8 w-3/6"
+    <nav
+      className="fixed bottom-24 left-0 right-0 bg-nav h-12 min-w-56 max-w-72 rounded-full mx-auto py-1"
     >
-      <div>
-        <svg
-          className="2xl:w-9 2xl:h-9
-            xl:w-8 xl:h-8
-            lg:w-6 lg:h-6
-            md:w-5 md:h-5
-            sm:w-5 sm:h-5
-            w-4 h-4"
-          width="31"
-          height="31"
-          viewBox="0 0 31 31"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M15.5 18.067C17.6686 18.067 19.4294 19.8254 19.4294 21.9963V30.1567H11.5683V21.9963C11.5683 19.8277 13.3291 18.067 15.5 18.067Z"
-            fill="white"
-          />
-          <path
-            d="M19.4294 30.1567V21.9986C19.4422 21.4744 19.35 20.9528 19.1583 20.4647C18.9665 19.9766 18.679 19.5318 18.3128 19.1565C17.9465 18.7812 17.5089 18.4829 17.0256 18.2792C16.5424 18.0756 16.0232 17.9707 15.4988 17.9707C14.9744 17.9707 14.4553 18.0756 13.972 18.2792C13.4887 18.4829 13.0511 18.7812 12.6849 19.1565C12.3186 19.5318 12.0311 19.9766 11.8394 20.4647C11.6476 20.9528 11.5554 21.4744 11.5683 21.9986V30.1567"
-            stroke="#FBFBFB"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M0.965864 25.3576L0.843292 15.7499C4.56994 9.66137 7.61065 6.1728 14.2036 1.64001C14.5849 1.37749 15.037 1.23694 15.5 1.23694C15.963 1.23694 16.4151 1.37749 16.7964 1.64001C23.3894 6.17044 26.4301 9.65901 30.1567 15.7475L30.0365 25.3552C29.9092 26.452 29.4154 27.4736 28.6351 28.2548C27.8548 29.0359 26.8337 29.5308 25.7371 29.6594C18.9335 30.4183 12.0666 30.4183 5.26294 29.6594C4.16676 29.5303 3.14618 29.0352 2.36635 28.2541C1.58653 27.473 1.09308 26.4516 0.965864 25.3552V25.3576Z"
-            stroke="#FBFBFB"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </div>
-      <div>
-        <svg
-          className="2xl:w-9 2xl:h-9
-            xl:w-8 xl:h-8
-            lg:w-7 lg:h-7
-            md:w-5 md:h-5
-            sm:w-5 sm:h-5
-            w-4 h-4"
-          width="33"
-          height="33"
-          viewBox="0 0 33 33"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M16.479 21.101C11.1611 21.101 6.61964 21.9051 6.61964 25.1252C6.61964 28.3453 11.1323 29.1782 16.479 29.1782C21.797 29.1782 26.3371 28.3728 26.3371 25.154C26.3371 21.9352 21.8258 21.101 16.479 21.101Z"
-            stroke="#FBFBFB"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M16.479 16.5081C19.9689 16.5081 22.7975 13.6782 22.7975 10.1884C22.7975 6.69847 19.9689 3.8699 16.479 3.8699C12.9892 3.8699 10.1593 6.69847 10.1593 10.1884C10.1475 13.6665 12.9577 16.4963 16.4345 16.5081H16.479Z"
-            stroke="#FBFBFB"
-            stroke-width="1.42857"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </div>
-      <div>
-        <svg
-          className="2xl:w-9 2xl:h-9
-            xl:w-8 xl:h-8
-            lg:w-7 lg:h-7
-            md:w-5 md:h-5
-            sm:w-5 sm:h-5
-            w-4 h-4"
-          width="33"
-          height="33"
-          viewBox="0 0 33 33"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M16.4933 22.9306V19.4422"
-            stroke="#FBFBFB"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M25.0111 7.32901C27.3349 7.32901 29.2049 9.21276 29.2049 11.5365V16.2665C25.8224 18.2465 21.3536 19.4428 16.4861 19.4428C11.6186 19.4428 7.16361 18.2465 3.78111 16.2665V11.5228C3.78111 9.19901 5.66486 7.32901 7.98861 7.32901H25.0111Z"
-            stroke="#FBFBFB"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M21.3058 7.32298V6.81973C21.3058 5.14223 19.9445 3.78098 18.267 3.78098H14.7195C13.042 3.78098 11.6808 5.14223 11.6808 6.81973V7.32298"
-            stroke="#FBFBFB"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M3.8148 21.2891L4.07467 24.739C4.25067 27.0641 6.18805 28.8612 8.51867 28.8612H24.4673C26.7979 28.8612 28.7353 27.0641 28.9113 24.739L29.1712 21.2891"
-            stroke="#FBFBFB"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </div>
-      <div>
-        <svg
-          className="2xl:w-9 2xl:h-9
-            xl:w-8 xl:h-8
-            lg:w-7 lg:h-7
-            md:w-5 md:h-5
-            sm:w-5 sm:h-5
-            w-4 h-4"
-          width="33"
-          height="33"
-          viewBox="0 0 33 33"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M15.8561 17.1495C21.3411 22.633 22.5854 16.2892 26.0778 19.7791C29.4446 23.145 31.3797 23.8194 27.114 28.084C26.5796 28.5134 23.1847 33.6796 11.2538 21.752C-0.678533 9.823 4.48468 6.42461 4.91421 5.89043C9.19032 1.61404 9.85306 3.5604 13.2199 6.92633C16.7123 10.4177 10.3712 11.6661 15.8561 17.1495Z"
-            stroke="#FBFBFB"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </div>
-      <div>
-        <h1
-          className="2xl:text-5xl font-thin text-[#6a6a6a] opacity-65
-          xl:text-3xl
-          lg:text-2xl
-          md:text-2xl"
-        >
-          |
-        </h1>
-      </div>
-      <div>
-        <h1 className="2xl:text-2xl xl:text-lg lg:text-sm md:text-[0.8rem] sm:text-[0.7rem] text-[0.6rem]">
-          EN
-        </h1>
-      </div>
-    </div>
+      <ul className="flex justify-around">
+        {navs.map((nav, index) => {
+          return (
+            <div key={`${nav.name}-${index}`}>
+              <img className="h-8 w-h-8" src={nav.icon} alt={nav.name} />
+            </div>
+          );
+        })}
+      </ul>
+
+    
+    </nav>
   );
 };
 
