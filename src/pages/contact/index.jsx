b729ffd1830f35b0a638f86fb2a5a568a66e68d5
pejmannn <pejmanxanii@gmail.com>
2024-03-24 06:58:31 -0700

    add formik

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,15 +1,50 @@
 import { ContactStyle } from "./style";
 import { useRef } from "react";
 import emailjs from '@emailjs/browser';
+import { useFormik } from "formik";
 
 const Contact = () => {
   const form = useRef()
+  const validate = values => {
+    const errors = {}
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+    if(!values.user_name){
+      errors.user_name = 'Required'
+    }else if(values.user_name.length > 0){
+      errors.user_name = 'please fill the name'
+    }
 
-    emailjs
-      .sendForm('service_31onzql', 'template_9msm2hc', form.current, {
+    if(!values.user_email){
+      errors.user_email = 'Required'
+    }else if(values.user_email.length < 4){
+      errors.user_email = 'must be 5 characters or more'
+    }
+  
+
+  if(!values.subject){
+    errors.subject = 'Required'
+  }else if(values.subject.length > 0){
+    errors.subject = 'please fill the name'
+  }
+
+  if(!values.message){
+    errors.message = 'Required'
+  }else if(values.message.length > 0){
+    errors.message = 'please fill the name'
+  }
+  return errors
+}
+  const formik = useFormik({
+    initialValues:{
+      user_name:'',
+      user_email:'',
+      subject:'',
+      message:''
+    },
+    onSubmit: value => {
+      alert(JSON.stringify(value, null, 2))
+      emailjs
+      .sendForm('service_31onzql', 'template_9msm2hc', value, {
         publicKey: '0n6P60s_euTh2aSDj',
       })
       .then(
@@ -19,6 +54,12 @@ const Contact = () => {
           console.log('FAILED...', error.text);
         },
       );
+    }
+  })
+
+
+
+    
   };
 
   return (
@@ -40,10 +81,12 @@ const Contact = () => {
         </div>
         <div>
           <form className="flex flex-col gap-4"
-          ref={form} onSubmit={sendEmail}
+          ref={form} onSubmit={formik.handleSubmit}
           >
             <input
               className={ContactStyle.input}
+              onChange={formik.handleChange}
+              value={formik.values.user_name}
               type="text"
               placeholder="Name"
               name="user_name"
@@ -51,18 +94,24 @@ const Contact = () => {
           
             <input
               className={ContactStyle.input}
+              onChange={formik.handleChange}
+              value={formik.values.user_email}
               type="email"
               placeholder="Email"
               name="user_email"
             />
             <input
               className={ContactStyle.input}
+              onChange={formik.handleChange}
+              value={formik.values.subject}
               type="text"
               placeholder="Subject"
               name="subject"
             />
             <textarea
               className={ContactStyle.textarea}
+              onChange={formik.handleChange}
+              value={formik.values.user_message}
               name="message"
               id=""
               cols="30"
